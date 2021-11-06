3b0056c562a9dd689c1f8a8d0fe99a61e6f3fbfe
Kristoffer <kjohansson93@hotmail.se>
2021-11-06 21:36:14 +0100

    Folder structure added

---
+++ SolarSimulation_backend/Services/IServices/IPlanetService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarSimulation_backend.Services.IServices
+{
+    public class IPlanetService
+    {
+    }
+}
